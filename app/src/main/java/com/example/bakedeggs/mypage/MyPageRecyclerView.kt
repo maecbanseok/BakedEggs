1cf0a0e030406d42ec7254783d21db3be8d6ec6b
APapeIsName <gms72901217@gmail.com>
2024-07-23 13:39:32 +0900

    init

---
+++ app/src/main/java/com/example/bakedeggs/mypage/MyPageRecyclerView.kt
@@ -0,0 +1,8 @@
+package com.example.bakedeggs.mypage
+
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class MyPageRecyclerView : ListAdapter<MyPageDataModel, RecyclerView.ViewHolder<>> {
+
+}
