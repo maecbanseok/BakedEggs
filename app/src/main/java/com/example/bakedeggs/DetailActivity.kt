1bdaf9493f1531e7288c40fbebd8dd5edc59ff49
rizzrian <rianni0505@gmail.com>
2024-07-26 16:28:17 +0900

    callMassageDetail

--- app/src/main/java/com/example/bakedeggs/DetailActivity.kt
+++ app/src/main/java/com/example/bakedeggs/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bakedeggs
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -31,6 +32,18 @@ class DetailActivity : AppCompatActivity() {
         binding.detailIvProfile.clipToOutline = true
         var uri: Uri = adList.get(Random.nextInt(adList.size))
         binding.DetailVvAd.setVideoURI(uri)
+
+        val tel = "010-0000-0000"
+
+        binding.detailIvCall.setOnClickListener{
+            val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tel))
+            startActivity(intent)
+        }
+
+        binding.detailIvMessage.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("smsto:" + tel))
+            startActivity(intent)
+        }
 
     }
 
